10e34cbf8d51b63f8de3dd661dcbf67cff5fa655
resparx <ramprash.rspx@gmail.com>
2020-04-14 01:57:52 +0530

    add mongo instance to docker

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,28 @@
 import http from 'http';
 import express from 'express';
+import mongoose from 'mongoose';
 import { applyMiddleware, applyRoutes } from './utils';
 import middleware from './middleware';
 import errorHandlers from "./middleware/errorHandler"
 import routes from "./services";
 
-process.on("uncaughtException", e => {
-  console.log(e);
-  process.exit(1);
-});
-process.on("unhandledRejection", e => {
-  console.log(e);
-  process.exit(1);
-});
+// process.on("uncaughtException", e => {
+//   console.log(e);
+//   process.exit(1);
+// });
+// process.on("unhandledRejection", e => {
+//   console.log(e);
+//   process.exit(1);
+// });
 
 const router = express();
+
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_PATH,
+} = process.env;
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}${MONGO_PATH}`)
 
 applyMiddleware(middleware, router);
 applyRoutes(routes, router);
